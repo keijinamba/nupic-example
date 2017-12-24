95cc776b1f078a980cc9faf08913777cca42fc74
keijinamba <keiji_1110@yahoo.co.jp>
2017-12-24 15:04:11 +0900

    Change some code of the generator file

--- SinewavePrediction/generate_data.py
+++ SinewavePrediction/generate_data.py
@@ -3,21 +3,31 @@
 import csv
 import math
 
-ROWS = 1000
 
-def run(filename="sine.csv"):
-	fileHandle = open(filename, 'w')
-	writer = csv.writer(fileHandle)
-	writer.writerow(['angle', 'sine'])
-	writer.writerow(['float', 'float'])
-	writer.writerow(['', ''])
 
-	for i in range(ROWS):
-		angle = (i * math.pi) / 50.0
-		sine_value = math.sin(angle)
-		writer.writerow([angle, sine_value])
+def generateData(num, file_name):
+  '''
+  Generate Sine wave data.
 
-	fileHandle.close()
+  Args:
+    num: number of data.
+    file_name: output csv file name.
+  '''
+
+  file_handler = open(file_name, 'w')
+  writer = csv.writer(file_handler)
+  writer.writerow(['angle', 'sine'])
+  writer.writerow(['float', 'float'])
+  writer.writerow(['', ''])
+
+  for i in range(num):
+    angle = i * math.pi / 50.0
+    sine_value = math.sin(angle)
+    writer.writerow([angle, sine_value])
+
+  file_handler.close()
+
+
 
 if __name__ == '__main__':
-	run()
+  generateData(num=1000, file_name="sine.csv")
